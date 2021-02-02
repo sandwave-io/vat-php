5faad0eb65164ccd779348be60694caa9236ec4f
Jesse Kramer <jesse@kramerventures.nl>
2021-02-02 16:43:04 +0100

    Fix @covers notation issue

--- tests/TempTest.php
+++ tests/TempTest.php
@@ -5,6 +5,7 @@ namespace SandwaveIo\Vat\Tests;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
+/** @covers \SandwaveIo\Vat\Vat */
 class TempTest extends TestCase
 {
     public function testExample(): void
