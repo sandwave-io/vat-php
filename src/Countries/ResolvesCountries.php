9f451af335ee33cfa0a8b115558d89e77fe7fcee
Jesse Kramer <jesse@kramerventures.nl>
2021-02-05 10:06:47 +0100

    Patch codestyle

--- src/Countries/ResolvesCountries.php
+++ src/Countries/ResolvesCountries.php
@@ -1,8 +1,10 @@
-<?php
+<?php declare(strict_types = 1);
+
 namespace SandwaveIo\Vat\Countries;
 
 interface ResolvesCountries
 {
     public function isCountryValid(string $countryCode): bool;
+
     public function isCountryInEu(string $countryCode): bool;
 }
