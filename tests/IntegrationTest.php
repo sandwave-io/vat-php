6bfa4668f38d2bdf3fd32b824443647f1516c6cc
Daan Barbiers <daan@sandwave.io>
2021-02-05 14:12:56 +0100

    Run integration tests sequentially.

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -8,6 +8,7 @@ use SandwaveIo\Vat\Vat;
 
 /**
  * This test actually talks to the remote API.
+ * The remote API is aggressively rate-limited so the tests are run sequentially.
  *
  * @coversNothing
  * @large
@@ -24,22 +25,29 @@ final class IntegrationTest extends TestCase
 
     public function testEuropeanVatRate(): void
     {
+        sleep(2);
         Assert::assertSame(21.0, $this->service->europeanVatRate('NL'));
     }
 
+    /** @depends testEuropeanVatRate */
     public function testNonEuropeanVatRate(): void
     {
+        sleep(2);
         Assert::assertSame(0.0, $this->service->europeanVatRate('OM'));
     }
 
+    /** @depends testNonEuropeanVatRate */
     public function testEuropeanVatNumber(): void
     {
+        sleep(2);
         Assert::assertTrue($this->service->validateEuropeanVatNumber('NL861350480B01', 'NL'));
         Assert::assertFalse($this->service->validateEuropeanVatNumber('NL138250460B01', 'NL'));
     }
 
+    /** @depends testEuropeanVatNumber */
     public function testNonEuropeanVatNumber(): void
     {
+        sleep(2);
         Assert::assertFalse($this->service->validateEuropeanVatNumber('NL861350480B01', 'OM'));
     }
 }
